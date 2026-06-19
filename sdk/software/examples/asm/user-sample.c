4233250367def62491a3ebe2f78529479e265ec1
CICC1002311Rg <cicc1002311rg@example.com>
2026-06-19 08:47:01 +0000

    替换 user-sample.c

--- sdk/software/examples/asm/user-sample.c
+++ sdk/software/examples/asm/user-sample.c
@@ -1,3 +1,4 @@
+#include "core_time.h"
 #include "led.h"
 #include "matmul.h"
 
@@ -187,7 +188,7 @@ int main(void)
     }
 
   //  commit_result_region(dst_base_word);
-    delay(0.1);
+    delay_ms(1);
     crc32 = compute_result_crc32(dst_base_word);
     uart_puts_blocking("MATMUL_CRC32=");
     uart_put_hex8(crc32);
